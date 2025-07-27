d3768e50ba714272584e3080ff8612be4e80e8a4
BenjamimGazane <114654552+BenjamimGazane@users.noreply.github.com>
2025-07-27 16:45:12 +0200

    Update formUtils.js

--- src/utils/formUtils.js
+++ src/utils/formUtils.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API = 'https://api-nodejs-express-prisma-mongo-db.onrender.com/produto';
+const API = 'https://api-nodejs-express-prisma-mongo-db.onrender.com/';
 
 export const criar = async (objeto) => axios.post(API, objeto);
 
